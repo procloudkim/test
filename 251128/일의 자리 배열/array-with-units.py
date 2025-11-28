7a70e821619921d432705532268d249406cc51e8
ProC <pro.cloud.kim@gmail.com>
2025-11-28 10:32:11 +0900

    Title: 일의 자리 배열, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/일의 자리 배열/array-with-units.py
+++ 251128/일의 자리 배열/array-with-units.py
@@ -2,10 +2,9 @@ a, b = map(int, input().split())
 
 arr = [a, b]          # 1항, 2항
 
-for i in range( ??? ):      # 몇 번 더 만들어야 하지?
-    next_val = ???          # 전전항 + 전항의 일의 자리
+for i in range(8):    # 이미 2개 있으니, 8개만 더 만들면 10개
+    next_val = (arr[-2] + arr[-1]) % 10  # 전전항 + 전항의 일의 자리
     arr.append(next_val)
 
-# 여기서 arr에는 10개가 들어있어야 함
 for x in arr:
     print(x, end=" ")
